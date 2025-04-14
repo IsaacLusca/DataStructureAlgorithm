a35c7d332997e37ac8bb7eae97012c197d5a74f7
IsaacLusca <isaaclucas.bezerra@gmail.com>
2025-04-14 09:24:38 -0300

    constructor da lista vinculada

---
+++ 03-LinkedList/00-under.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int value;
+        Node* next;
+
+        Node(int value) {
+            this->value = value;
+            // this->next = nullptr;
+            next = nullptr;
+        }
+};
+
+class LinkedList {
+    // primeiros atributos/variaveis
+    private:
+        Node* head;
+        Node* tail;
+        int length;
+
+    // Constructor
+    LinkedList(int value) {
+        Node* newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+};
+
+int main() {
+
+
+
+    return 0;
+}
