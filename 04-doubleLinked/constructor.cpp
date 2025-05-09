80f0acc246785cf8f853cf526b36c61cc87c4ba1
IsaacLusca <isaaclucas.bezerra@gmail.com>
2025-05-09 19:05:39 -0300

    finalizando o constructor.

--- 04-doubleLinked/constructor.cpp
+++ 04-doubleLinked/constructor.cpp
@@ -15,6 +15,33 @@ class Node {
         }
 };
 
-int main() {      
+class DoublyLinkedList {
+    private:
+        Node* head;
+        Node* tail;
+        int length;
 
+    public:
+        DoublyLinkedList(int value) {
+            Node* newNode = new Node(value);
+            head = newNode;
+            tail = newNode;
+            length = 1;
+        }
+
+        void printList() {
+            Node* temp = head;
+            while(temp) {
+                cout << temp->value << " ";
+                temp = temp->next;       
+            }
+        }
+};
+
+int main() {      
+    DoublyLinkedList* myDLL = new DoublyLinkedList(7);
+
+    myDLL->printList();
+
+    return 0;
 }
