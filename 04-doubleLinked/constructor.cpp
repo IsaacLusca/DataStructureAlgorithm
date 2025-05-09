a957ccbcd55186b26901d2364161f1c2f6c27e21
IsaacLusca <isaaclucas.bezerra@gmail.com>
2025-05-09 18:52:37 -0300

    Constructor do nó da double linked list

---
+++ 04-doubleLinked/constructor.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int value;
+        Node* next;
+        Node* prev;
+        
+        Node(int value) {
+            this->value = value;
+            next = nullptr;
+            prev = nullptr;
+        }
+};
+
+int main() {      
+
+}
