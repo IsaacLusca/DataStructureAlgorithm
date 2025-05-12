f6ac92d55e9074b7f02b9900d5981a97bc6fc8f3
IsaacLusca <isaaclucas.bezerra@gmail.com>
2025-05-12 17:30:40 -0300

    func push: adicionar nó ao topo

---
+++ 05-stacks/01-push.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int value;
+        Node* next;
+
+        Node(int value) {
+            this->value = value;
+            next = nullptr;
+        }
+};
+
+class Stack {
+    private:
+        Node* top;
+        int height;
+
+    public:
+        Stack(int value) {
+            Node* newNode = new Node(value);
+            top = newNode;
+            height = 1;
+        }
+        
+        void printStack() {
+            Node* temp = top;
+            while(temp) {
+                cout << " " <<temp->value << endl;
+                cout << " \\/" << endl;
+                temp = temp->next;
+            }
+        }
+
+        void pop(int value) {
+            Node* newNode = new Node(value);
+            newNode->next = top;
+            top = newNode;
+            height++;
+        }
+};
+
+int main() {
+
+    Stack* newStack = new Stack(4);
+
+    newStack->pop(5);
+    newStack->pop(7);
+    newStack->pop(10);
+
+    newStack->printStack();
+
+    return 0;
+}
